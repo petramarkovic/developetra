139ee28864b46aa0dca346c9afb2a1191b18bcb8
Petra Markovic <petra.markovic@vegait.rs>
2023-08-28 13:44:06 +0200

    Add webpack config files

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,11 @@ const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-	mode: 'production',
-	devtool: 'source-map',
-});
+	mode: 'development',
+	devtool: 'inline-source-map',
+	devServer: {
+		port: 3000,
+		hot: true,
+		static: './dist',
+	},
+  });
