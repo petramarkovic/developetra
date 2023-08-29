5a93e78191e71212fd9062abac8dbb057f5f5d21
Petra Markovic <petra.markovic@vegait.rs>
2023-08-29 10:40:04 +0200

    Disallow indexing

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.js',
